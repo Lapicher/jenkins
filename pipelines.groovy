657e42984eb8a0ea31942e28d58c81430540a58d
Nataly Contreras <lapicher_j@webadictos.net>
2020-06-10 15:11:13 -0500

    without conditional to evaluate

--- pipelines.groovy
+++ pipelines.groovy
@@ -53,14 +53,7 @@ echo ${params.BUILD_NOTES}
 String klocwork_project = 'NADA AUN MAP'
 String klocwork_project_boot = ''
 String klocwork_server = ''
-/*
-if(project_map.containsKey(params.BUILD_CONFIG))
-{
-    klocwork_project = project_map["${params.BUILD_CONFIG}"]['project_vuc'],
-    // klocwork_project_boot = klocwork.project_map["${params.BUILD_CONFIG}"]['project_boot'],
-    // klocwork_server = klocwork.project_map["${params.BUILD_CONFIG}"]['server'],
-}
-*/
+
 print "MAP: "
 print project_map
 print "INFO: klocwork project ${klocwork_project}"
